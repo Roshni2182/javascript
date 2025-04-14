eff868effb6a577962e2489251ff0a987a5716fc
Roshni2182 <roshniparween377@gmail.com>
2025-04-14 07:10:48 +0000

    dataType

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,26 @@
+"use strict" ; //treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs , not browser
+
+console.log(3+3)
+
+console.log("roshni")
+
+let age=19
+let name="roshni"
+let loggIn=true
+
+// number = 2 to the power 56
+//string =""
+//boolean = true/false 
+// bigInt
+//null = standalone value
+//undefined
+//symbol = unique
+
+
+// object
+
+console.log(typeof "roshni")
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
