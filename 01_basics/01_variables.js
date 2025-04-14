98c94fadb5a2f07777043c09435264cecf182d43
Roshni2182 <roshniparween377@gmail.com>
2025-04-14 07:10:29 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144223
+let accountEmail="roshni@google.com"
+var sccountPassword="34254"
+accountCity="Goa"
+let accountState;
+//accountId=3 //not use
+
+/*
+prefer not to use var
+because of issue in block scope and function scope
+*/
+
+accountEmail="24@23"
+accountPassword="4556"
+accountCity="bokaro"
+
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity,accountState])
